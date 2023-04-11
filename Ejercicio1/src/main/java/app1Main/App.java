102c9867894c0cc6a170c7fcc3cc58cb69397d51
Paula Sousa <paulasousa137@gmail.com>
2023-04-11 11:36:00 +0200

    conector DB

--- Ejercicio1/src/main/java/app1Main/App.java
+++ Ejercicio1/src/main/java/app1Main/App.java
@@ -9,7 +9,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
         Connector c = new Connector();
+        c.createDB("servicio");
+        c.createTable(null, null, null);
     }
 }
--- Ejercicio1/src/main/java/models/Models.java
+++ Ejercicio1/src/main/java/models/Models.java
@@ -1,6 +1,8 @@
 package models;
 
+
 public class Models {
+
 
 
 }
