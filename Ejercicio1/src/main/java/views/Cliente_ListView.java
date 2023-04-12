e96393b34d32e1122af523aaea1bd94a98df62dd
JPrimee <javierpsantos99@gmail.com>
2023-04-12 15:01:40 +0200

    Vista de creación creada

---
+++ Ejercicio1/src/main/java/views/Cliente_ListView.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package views;
+
+import javax.swing.*;
+
+/**
+ * @author Javier
+ *
+ */
+public class Cliente_ListView extends JFrame{
+
+	private JPanel contentPane;
+	
+	public Cliente_ListView() {
+		initComponents();
+	}
+	
+	private void initComponents() {
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+}
