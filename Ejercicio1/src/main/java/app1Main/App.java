ac694573da6218c4be9c7bc9684d24f5ca177658
Paula Sousa <paulasousa137@gmail.com>
2023-04-12 20:11:21 +0200

    Panel General

--- Ejercicio1/src/main/java/app1Main/App.java
+++ Ejercicio1/src/main/java/app1Main/App.java
@@ -2,6 +2,7 @@ package app1Main;
 
 import javax.swing.*;
 import controller.*;
+import views.Cliente_Index;
 import views.Cliente_InsertView;
 import views.Cliente_ListView;
 
@@ -24,11 +25,14 @@ public class App {
     	catch (IllegalAccessException e) {	
     	}
     	
-    	Cliente_InsertView insertView = new Cliente_InsertView();
-    	Cliente_InsertController controllerInsert = new Cliente_InsertController(insertView);
-    	controllerInsert.initVista();
-    	Cliente_ListView listView = new Cliente_ListView();
-    	Cliente_ListController controller = new Cliente_ListController(listView);
-    	controller.initVista();
+//    	Cliente_InsertView insertView = new Cliente_InsertView();
+//    	Cliente_InsertController controllerInsert = new Cliente_InsertController(insertView);
+//    	controllerInsert.initVista();
+//    	Cliente_ListView listView = new Cliente_ListView();
+//    	Cliente_ListController controller = new Cliente_ListController(listView);
+//    	controller.initVista();
+    	Cliente_Index insertIndex = new Cliente_Index();
+		Cliente_IndexController controller = new Cliente_IndexController(insertIndex);
+		controller.initVista();	
     }
 }
---
+++ Ejercicio1/src/main/java/views/Cliente_Index.java
@@ -0,0 +1,82 @@
+package views;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+
+public class Cliente_Index extends JFrame {
+
+	public JPanel contentPane;
+	public JButton nuevoCliente;
+	public JButton editarCliente;
+	public JButton verCliente;
+	public JButton eliminarCliente;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Cliente_Index frame = new Cliente_Index();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	
+	public Cliente_Index() {
+		initComponents();
+	}
+	
+	public void initComponents() {
+		
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(267, 14, 1, 1);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblBienvenido = new JLabel("Gestor de Clientes");
+		lblBienvenido.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBienvenido.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblBienvenido.setBounds(79, 25, 340, 62);
+		contentPane.add(lblBienvenido);
+		
+		nuevoCliente = new JButton("Añadir Cliente");
+		nuevoCliente.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		nuevoCliente.setBounds(185, 121, 144, 40);
+		contentPane.add(nuevoCliente);
+		
+		editarCliente = new JButton("Editar Cliente");
+		editarCliente.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		editarCliente.setBounds(185, 187, 144, 40);
+		contentPane.add(editarCliente);
+		
+		verCliente = new JButton("Ver Clientes");
+		verCliente.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		verCliente.setBounds(185, 249, 144, 40);
+		contentPane.add(verCliente);
+		
+		eliminarCliente = new JButton("Eliminar Cliente");
+		eliminarCliente.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		eliminarCliente.setBounds(185, 310, 144, 40);
+		contentPane.add(eliminarCliente);
+	}
+}
