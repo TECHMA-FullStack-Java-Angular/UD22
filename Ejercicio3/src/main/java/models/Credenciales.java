ced67cca269765d9d04df50d650f093e53e74a18
Joan <joan.lapeyra@estudiantat.upc.edu>
2023-04-24 20:05:39 +0200

    credenciales

---
+++ Ejercicio3/src/main/java/models/Credenciales.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Credenciales {
+	public static final String URL = "jdbc:mysql://192.168.1.177:3306";
+    public static final String USER = "java";
+    public static final String PASSWORD = "Java_R00t";
+}
